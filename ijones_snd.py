dd528856852720a7e1a19704a9edccc2f387e370
fdotta@github.com <fdotta@gmail.com>
2014-08-11 19:59:01 -0300

    code clean up close issues #8

--- ijones_snd.py
+++ ijones_snd.py
@@ -31,60 +31,9 @@ class bcolors:
     highlightred     = '\033[1;48m'
     reset            = '\033[0m'
 
-class progressbarClass:
-    def __init__(self, finalcount, progresschar=None):
-        import sys
-        self.finalcount=finalcount
-        self.blockcount=0
-        #
-        # See if caller passed me a character to use on the
-        # progress bar (like "*").  If not use the block
-        # character that makes it look like a real progress
-        # bar.
-        #
-        if not progresschar: self.block=chr(178)
-        else:                self.block=progresschar
-        #
-        # Get pointer to sys.stdout so I can use the write/flush
-        # methods to display the progress bar.
-        #
-        self.f=sys.stdout
-        #
-        # If the final count is zero, don't start the progress gauge
-        #
-        if not self.finalcount : return
-#        self.f.write('\n------------------ % Progress -------------------1\n')
-#        self.f.write('    1    2    3    4    5    6    7    8    9    0\n')
-#        self.f.write('----0----0----0----0----0----0----0----0----0----0\n')
-        return
 
-    def progress(self, count):
-        #
-        # Make sure I don't try to go off the end (e.g. >100%)
-        #
-        count=min(count, self.finalcount)
-        #
-        # If finalcount is zero, I'm done
-        #
-        if self.finalcount:
-            percentcomplete=int(round(100*count/self.finalcount))
-            if percentcomplete < 1: percentcomplete=1
-        else:
-            percentcomplete=100
 
-        #print "percentcomplete=",percentcomplete
-        blockcount=int(percentcomplete/2)
-        #print "blockcount=",blockcount
-        if blockcount > self.blockcount:
-            for i in range(self.blockcount,blockcount):
-                self.f.write(self.block)
-                self.f.flush()
-
-        if percentcomplete == 100: self.f.write(bcolors.green+" [Done]\n")
-        self.blockcount=blockcount
-        return
-
-class ProgressBar1:
+class ProgressBar:
     def __init__(self, duration):
         self.duration = duration
         self.prog_bar = '[]'
@@ -326,29 +275,20 @@ def seek(sources,sqlobj):
         filelist = get_filelist(source)
         count = 0           # Progress Bar counter
 
-# -=- Progress Bar 1 (class progressbarClass) -=-----------------------
-#        pb=progressbarClass(len(filelist),bcolors.yellow+"*")
-# -=- End Progress Bar 1 -=--------------------------------------------
-
-# -=- Progress Bar 2 (class ProgressBar1) -=---------------------------
-        p = ProgressBar1(len(filelist))
+# -=- Progress Bar (class ProgressBar1) -=---------------------------
+        p = ProgressBar(len(filelist))
         p.fill_char = '='
 # -=- End Progress Bar 2 ----------------------------------------------
-
         for filepath in filelist:
             count+=1           # Increment for progress bar count
-# -=- Progress Bar 1 (class progressbarClass) -=-----------------------
-#            pb.progress(count)
-# -=- End Progress Bar 1
-
-# -=- Progress Bar 2 (class ProgressBar1) -=---------------------------
+# -=- Progress Bar (class ProgressBar1) -=---------------------------
             str1 = bcolors.green+"    Seeking in"+bcolors.red+" %s "%source
             str2 = str1+bcolors.green+"............. "+bcolors.yellow
             p.update_time(count)
             sys.stdout.write("\r")
             sys.stdout.write("%s%s" % (str2, p))
             sys.stdout.flush()
-# -=- End Progress Bar 2 -=-------------------------------------------
+# -=- End Progress Bar -=-------------------------------------------
             filename = os.path.split(filepath)[1]
             update = False
             logger.debug("messing with file: %s" % filename)
