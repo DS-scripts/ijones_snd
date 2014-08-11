3ca5db081e805583ea6cc88cea0ac819e20de969
fdotta@github.com <fdotta@gmail.com>
2014-08-11 00:39:19 -0300

    + Add some user message (seek only for now) + Add progress bar (seek only for now)

--- ijones_snd.py
+++ ijones_snd.py
@@ -13,6 +13,112 @@ from optparse import OptionParser,OptionGroup
 sqlitepath = "~/.ijones/p.sqlite"
 program_name    = "IJ SND"
 
+
+# -=-[STDOUT Colors]-=-
+class bcolors:
+    gray             = '\033[1;30m'
+    red              = '\033[1;31m'
+    green            = '\033[1;32m'
+    yellow           = '\033[1;33m'
+    blue             = '\033[1;34m'
+    magenta          = '\033[1;35m'
+    cyan             = '\033[1;36m'
+    white            = '\033[1;37m'
+    highlightgreen   = '\033[1;42m'
+    highlightblue    = '\033[1;44m'
+    highlightmagenta = '\033[1;45m'
+    highlightcyan    = '\033[1;46m'
+    highlightred     = '\033[1;48m'
+    reset            = '\033[0m'
+
+class progressbarClass:
+    def __init__(self, finalcount, progresschar=None):
+        import sys
+        self.finalcount=finalcount
+        self.blockcount=0
+        #
+        # See if caller passed me a character to use on the
+        # progress bar (like "*").  If not use the block
+        # character that makes it look like a real progress
+        # bar.
+        #
+        if not progresschar: self.block=chr(178)
+        else:                self.block=progresschar
+        #
+        # Get pointer to sys.stdout so I can use the write/flush
+        # methods to display the progress bar.
+        #
+        self.f=sys.stdout
+        #
+        # If the final count is zero, don't start the progress gauge
+        #
+        if not self.finalcount : return
+#        self.f.write('\n------------------ % Progress -------------------1\n')
+#        self.f.write('    1    2    3    4    5    6    7    8    9    0\n')
+#        self.f.write('----0----0----0----0----0----0----0----0----0----0\n')
+        return
+
+    def progress(self, count):
+        #
+        # Make sure I don't try to go off the end (e.g. >100%)
+        #
+        count=min(count, self.finalcount)
+        #
+        # If finalcount is zero, I'm done
+        #
+        if self.finalcount:
+            percentcomplete=int(round(100*count/self.finalcount))
+            if percentcomplete < 1: percentcomplete=1
+        else:
+            percentcomplete=100
+
+        #print "percentcomplete=",percentcomplete
+        blockcount=int(percentcomplete/2)
+        #print "blockcount=",blockcount
+        if blockcount > self.blockcount:
+            for i in range(self.blockcount,blockcount):
+                self.f.write(self.block)
+                self.f.flush()
+
+        if percentcomplete == 100: self.f.write(bcolors.green+" [Done]\n")
+        self.blockcount=blockcount
+        return
+
+class ProgressBar1:
+    def __init__(self, duration):
+        self.duration = duration
+        self.prog_bar = '[]'
+        self.fill_char = '#'
+        self.width = 40
+        self.__update_amount(0)
+
+    def animate(self):
+        for i in range(self.duration):
+            if sys.platform.lower().startswith('win'):
+                print self, '\r',
+            else:
+                print self, chr(27) + '[A'
+            self.update_time(i + 1)
+        print self
+
+    def update_time(self, elapsed_secs):
+        self.__update_amount((elapsed_secs / float(self.duration)) * 100.0)
+        self.prog_bar += '  %d/%s' % (elapsed_secs, self.duration)
+
+    def __update_amount(self, new_amount):
+        percent_done = int(round((new_amount / 100.0) * 100.0))
+        all_full = self.width - 2
+        num_hashes = int(round((percent_done / 100.0) * all_full))
+        self.prog_bar = '[' + self.fill_char * num_hashes + ' ' * (all_full - num_hashes) + ']'
+        pct_place = (len(self.prog_bar) / 2) - len(str(percent_done))
+        pct_string = '%d%%' % percent_done
+        self.prog_bar = self.prog_bar[0:pct_place] + \
+            (pct_string + self.prog_bar[pct_place + len(pct_string):])
+
+    def __str__(self):
+        return str(self.prog_bar)
+
+
 def parse():
     usage   = "usage: %prog [options] [SOURCE1] [SOURCE2] ..."
     epilogue = "When no source is defined, use ."
@@ -126,6 +232,11 @@ def get_md5(filepath):
         return stdoutsplit[0]
 
 def get_filelist(source):
+    sys.stdout.write("\r")
+    sys.stdout.write(bcolors.green+"    Getting file list in ")
+    sys.stdout.write(bcolors.red+"%s" % source)
+    sys.stdout.write(bcolors.green+" ... ")
+    sys.stdout.flush()
     filelist = []
     for r,d,f in os.walk(source):
         logger.debug("Walking into: %s" % r)
@@ -143,6 +254,9 @@ def get_filelist(source):
                 logger.warning("%s is link. Not following" % filepath)
                 continue
             filelist.append(filepath)
+    sys.stdout.write(bcolors.white+"["+bcolors.blue+"done"+bcolors.white+"]")
+    sys.stdout.flush()
+    print bcolors.reset
     return filelist
 
 def fix_filepath(filepath):
@@ -199,10 +313,41 @@ def destroy(sources,dryrun,sqlobj):
 
 def seek(sources,sqlobj):
     logger.debug("Sources selected: %s" % sources)
+    print bcolors.cyan+">>> Seeking Mode "+bcolors.white+"["+bcolors.highlightmagenta+"enabled"+bcolors.reset+bcolors.white+"]"
     for source in sources:
+        sys.stdout.write("\r")
+        sys.stdout.write(bcolors.green+"    Seeking in ")
+        sys.stdout.write(bcolors.red+"%s" % source)
+        sys.stdout.write(bcolors.green+" ............. ")
+        sys.stdout.flush()
+
         logger.info("Seeking through %s" % source)
         filelist = get_filelist(source)
+        count = 0           # Progress Bar counter
+
+# -=- Progress Bar 1 (class progressbarClass) -=-----------------------
+#        pb=progressbarClass(len(filelist),bcolors.yellow+"*")
+# -=- End Progress Bar 1 -=--------------------------------------------
+
+# -=- Progress Bar 2 (class ProgressBar1) -=---------------------------
+        p = ProgressBar1(len(filelist))
+        p.fill_char = '='
+# -=- End Progress Bar 2 ----------------------------------------------
+
         for filepath in filelist:
+            count+=1           # Increment for progress bar count
+# -=- Progress Bar 1 (class progressbarClass) -=-----------------------
+#            pb.progress(count)
+# -=- End Progress Bar 1
+
+# -=- Progress Bar 2 (class ProgressBar1) -=---------------------------
+            str1 = bcolors.green+"    Seeking in"+bcolors.red+" %s "%source
+            str2 = str1+bcolors.green+"............. "+bcolors.yellow
+            p.update_time(count)
+            sys.stdout.write("\r")
+            sys.stdout.write("%s%s" % (str2, p))
+            sys.stdout.flush()
+# -=- End Progress Bar 2 -=-------------------------------------------
             filename = os.path.split(filepath)[1]
             update = False
             logger.debug("messing with file: %s" % filename)
@@ -229,6 +374,8 @@ def seek(sources,sqlobj):
             if not update:
                 sql = "INSERT INTO SND (PATH,MD5,SIZE,CTIME) VALUES ('%s','%s',%s,%s)" % (filepath,md5,size,int(ctime))
             sqlobj.execute(sql)
+        print (bcolors.reset+" ")
+    print (bcolors.reset+" ")
 
 def wipe(sqlobj):
     logger.info("Wiping database")
